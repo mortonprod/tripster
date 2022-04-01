7a6af44b02f4157446483cfc90036295622e0b70
mortonprod <mortonprod@gmail.com>
2022-04-01 22:03:11 +0100

    Include references. Format docs

--- README.md
+++ README.md
@@ -5,3 +5,13 @@ emcc --pre-js pre.js -o iframe.js src/hello/hello.c
 # Issues
 
 How do we get images in the iframe context when it will not have the access rights to get to that webpage? No access to cookies or JWT
+
+# Reference
+
+https://github.com/GoogleChromeLabs/wasm-av1
+https://web.dev/wasm-av1/
+https://github.com/dteare/wasm-csp
+https://github.com/inflatablegrade/Extension-with-WASM
+https://www.weforum.org/agenda/2021/06/deep-learning-animated-moving-photos/
+https://blog.tensorflow.org/2020/09/bringing-mona-lisa-effect-to-life-tensorflow-js.html
+https://surma.dev/things/c-to-webassembly/
--- pre.js
+++ pre.js
@@ -1,14 +1,14 @@
 var button = document.querySelector('.my-button');
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
     console.log('I have been clicked');
 });
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      console.log("iframe.js got a message")
-      console.log(request);
-      console.log(sender);
-    //   chrome.tabs.sendMessage(sender.tab.id, `Again ${request}`);
-      sendResponse("bar from iframe");
+    function (request, sender, sendResponse) {
+        console.log("iframe.js got a message")
+        console.log(request);
+        console.log(sender);
+        //   chrome.tabs.sendMessage(sender.tab.id, `Again ${request}`);
+        sendResponse("bar from iframe");
     }
-  );
+);

dff7dc49feb1edeac0afa60b28e249b7b2d0c2e9
mortonprod <mortonprod@gmail.com>
2022-04-13 21:19:07 +0100

    Remove unused code for current functionality

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -4,27 +4,20 @@ console.log("Content is up and running");
 document.onreadystatechange = () => {
   const imgs = getInfoImgs();
   for (const [key,value] of imgs.entries()) {
-    // let newDiv = document.createElement("div");
+    console.log(`Create iframe for image: ${key}`);
     const newIframe = document.createElement("iframe");
     newIframe.src = chrome.runtime.getURL(`iframe.html?id=${key}`);
     newIframe.id = key;
-    // newDiv.style.cssText = `top:${offset.top}px; left:${offset.left}px; width: ${size.width}px; height:${size.height}px; z-index:1000; position: absolute`
     newIframe.style.cssText = `padding: 0px; margin: 0px; border:0px; top:${value.offset.top}px; left:${value.offset.left}px; width: ${value.size.width}px; height:${value.size.height}px; z-index:1000; position: absolute`
-    // newDiv.appendChild(newIframe);
     document.body.appendChild(newIframe)
   }
   const resizedw = () => {
     const imgs = getInfoImgs();
     for (const [key,value] of imgs.entries()) {
+      console.log(`Resize iframe for image: ${key}`);
       const iFrame = document.getElementById(key);
-      // console.log(`iFrame blah: ${iFrame}`);
-      // iFrame.style.width = "100px";
       iFrame.style= `padding: 0px; margin: 0px; border:0px; top:${value.offset.top}px; left:${value.offset.left}px; width: ${value.size.width}px; height:${value.size.height}px; z-index:1000; position: absolute`
     }
-    console.log(`inside resize ${imgs}`);
-    // chrome.runtime.sendMessage(Object.fromEntries(imgs), (temp) => {
-    //   console.log(temp);
-    // })
   }
   // Need this or we resize repeatedly
   let doit;
@@ -32,18 +25,11 @@ document.onreadystatechange = () => {
     clearTimeout(doit);
     doit = setTimeout(resizedw, 100);
   };
-    // Need this or we scroll repeatedly
-  // let doitscroll;
-  // window.onscroll = function () {
-  //   console.log('scroll');
-  //   clearTimeout(doitscroll);
-  //   doit = setTimeout(resizedw, 100);
-  // };
   // TODO: Need to wait some time to ensure the iframes have been created
-  window.setTimeout(() => {
-    console.log(`inside timeout ${imgs}`);
-    chrome.runtime.sendMessage(Object.fromEntries(imgs), (temp) => {
-      console.log(temp);
-    })
-  }, 5000);
+  // window.setTimeout(() => {
+  //   console.log(`inside timeout ${imgs}`);
+  //   chrome.runtime.sendMessage(Object.fromEntries(imgs), (temp) => {
+  //     console.log(temp);
+  //   })
+  // }, 5000);
 }
--- src/pre.js
+++ src/pre.js
@@ -1,20 +1,24 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const img = new Image();
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
-        console.log(request)
-        console.log('Start breaking images into parts...')
-        console.log(`iframe id: ${window.location.search.substring(1).split('=')[1]}`);
-        let map = new Map(Object.entries(request));
-        let info = map.get(window.location.search.substring(1).split('=')[1]);
-        console.log(`Info ${JSON.stringify(info)}`);
-        img.src = "https://alexandermorton.co.uk/68cb5bcc4e0025874fdb34456d66d09f.jpg";
-        img.onload = () => {
-            ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
-        }
-    }
-);
+img.src = "https://alexandermorton.co.uk/68cb5bcc4e0025874fdb34456d66d09f.jpg";
+img.onload = () => {
+    ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+}
+// chrome.runtime.onMessage.addListener(
+//     function (request, sender, sendResponse) {
+//         console.log(request)
+//         console.log('Start breaking images into parts...')
+//         console.log(`iframe id: ${window.location.search.substring(1).split('=')[1]}`);
+//         let map = new Map(Object.entries(request));
+//         let info = map.get(window.location.search.substring(1).split('=')[1]);
+//         console.log(`Info ${JSON.stringify(info)}`);
+//         img.src = "https://alexandermorton.co.uk/68cb5bcc4e0025874fdb34456d66d09f.jpg";
+//         img.onload = () => {
+//             ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+//         }
+//     }
+// );
 
 // var button = document.querySelector('.my-button');
 // button.addEventListener('click', function () {

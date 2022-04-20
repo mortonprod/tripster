b6478626d686906850e4b40d53f2211ab7aa98b5
mortonprod <mortonprod@gmail.com>
2022-04-20 14:07:44 +0100

    Update popup and options page

--- src/popup.ts
+++ src/popup.ts
@@ -3,3 +3,10 @@ import '../styles/popup.scss';
 document.getElementById('go-to-options').addEventListener('click', () => {
   chrome.runtime.openOptionsPage();
 });
+
+const btn: HTMLButtonElement = document.querySelector('#btn');
+const sb: HTMLSelectElement = document.querySelector('#framework');
+btn.onclick = (event) => {
+    event.preventDefault();
+    console.log(sb.value);
+};

d8e242ccaffdc0ccd8a383f78a6d32bde53d2c70
mortonprod <mortonprod@gmail.com>
2022-04-20 22:24:03 +0100

    Set waves as default

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -2,14 +2,13 @@ import { scroll, deleteFrames, enableImgs, setType } from "./contentUtils";
 console.log("Content is up and running");
 
 document.onreadystatechange = () => {
-  // Scroll at the very start to we create the visible iframes
-  scroll();
   // Need this or we scroll repeatedly
   let doscroll: any;
   function delayedScroll() {
     clearTimeout(doscroll);
     doscroll = setTimeout(scroll, 500);
   }
+  scroll();
   window.addEventListener('scroll', delayedScroll);
   chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
     console.log(`Trip: ${request.trip}`);
